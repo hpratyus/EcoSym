faaeb064730205254ee1fe7fd0e73540ef47afc6
bubbl337 <79190899+bubbl337@users.noreply.github.com>
2022-09-05 05:55:30 +0545

    initial commit
    took a while to figure out how to get this thing up and running

---
+++ src/player.cs
@@ -0,0 +1,34 @@
+using Godot;
+using System;
+
+public class player : Sprite
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
+
+ // Called every frame. 'delta' is the elapsed time since the previous frame.
+ public override void _Process(float delta)
+
+ {
+     float AMOUNT = 5;
+         if(Input.IsKeyPressed((int)KeyList.W)){
+        this.Position += new Vector2(0,-AMOUNT);
+    }     
+        if(Input.IsKeyPressed((int)KeyList.S)){
+        this.Position += new Vector2(0,AMOUNT);
+    }     
+        if(Input.IsKeyPressed((int)KeyList.A)){
+        this.Position += new Vector2(-AMOUNT,0);
+    }     
+        if(Input.IsKeyPressed((int)KeyList.D)){
+        this.Position += new Vector2(AMOUNT,0);
+    }     
+ }
+}
